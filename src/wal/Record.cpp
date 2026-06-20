d47df55e59bc7b475ee76b65465e00019c4769c6
Robert Livengood <rlivengood6@gatech.edu>
2026-06-19 19:49:25 -0500

    Added CRC32 calculation and validation

--- src/wal/Record.cpp
+++ src/wal/Record.cpp
@@ -2,57 +2,74 @@
 #include <iostream>
 
 namespace {
-    void writeUint32(ostream& out, uint32_t value) {
-        const char bytes[4] = {
-            static_cast<char>((value >> 0) & 0xFF),
-            static_cast<char>((value >> 8) & 0xFF),
-            static_cast<char>((value >> 16) & 0xFF),
-            static_cast<char>((value >> 24) & 0xFF)
-        };
-
-        out.write(bytes, 4);
+    void serialize_uint32(uint32_t value, uint8_t bytes[4]) {
+        bytes[0] = static_cast<uint8_t>((value >> 0) & 0xFF);
+        bytes[1] = static_cast<uint8_t>((value >> 8) & 0xFF);
+        bytes[2] = static_cast<uint8_t>((value >> 16) & 0xFF);
+        bytes[3] = static_cast<uint8_t>((value >> 24) & 0xFF);
     }
 
-    void writeUint64(ostream& out, uint64_t value) {
-        const char bytes[8] = {
-            static_cast<char>((value >> 0) & 0xFF),
-            static_cast<char>((value >> 8) & 0xFF),
-            static_cast<char>((value >> 16) & 0xFF),
-            static_cast<char>((value >> 24) & 0xFF),
-            static_cast<char>((value >> 32) & 0xFF),
-            static_cast<char>((value >> 40) & 0xFF),
-            static_cast<char>((value >> 48) & 0xFF),
-            static_cast<char>((value >> 56) & 0xFF)
-        };
-
-        out.write(bytes, 8);
+    void serialize_uint64(uint64_t value, uint8_t bytes[8]) {
+        bytes[0] = static_cast<uint8_t>((value >> 0) & 0xFF);
+        bytes[1] = static_cast<uint8_t>((value >> 8) & 0xFF);
+        bytes[2] = static_cast<uint8_t>((value >> 16) & 0xFF);
+        bytes[3] = static_cast<uint8_t>((value >> 24) & 0xFF);
+        bytes[4] = static_cast<uint8_t>((value >> 32) & 0xFF);
+        bytes[5] = static_cast<uint8_t>((value >> 40) & 0xFF);
+        bytes[6] = static_cast<uint8_t>((value >> 48) & 0xFF);
+        bytes[7] = static_cast<uint8_t>((value >> 56) & 0xFF);
     }
 
-    void writeMagic(ostream& out) {
-        uint32_t magic = 0x314C4157; // WAL1 - litte endian
-        writeUint32(out, magic);
+    void write_uint32(ostream& out, uint8_t bytes[4]) {
+        out.write(reinterpret_cast<const char*>(bytes), 4);
     }
 
-    void writePayloadLen(ostream& out, const vector<uint8_t>& payload) {
-        uint32_t payloadLen = payload.size();
-        writeUint32(out, payloadLen);
+    void write_uint64(ostream& out, uint8_t bytes[8]) {
+        out.write(reinterpret_cast<const char*>(bytes), 8);
     }
 
-    void writeLsn(ostream& out, Lsn lsn) {
-        writeUint64(out, lsn);
+    uint32_t initCRC() {
+        return 0xFFFFFFFF;
     }
 
-    void writeCrc(ostream& out) {
-        // TODO - this is a temp CRC for now
-        uint32_t crc = 0x00000000;
-        writeUint32(out, crc);
+    uint32_t updateCRC(uint32_t crc, const uint8_t* data, size_t len) {
+        for (size_t i = 0; i < len; ++i) {
+            crc ^= data[i];
+
+            for (int bit = 0; bit < 8; ++bit) {
+                if (crc & 1) {
+                    crc = (crc >> 1) ^ 0xEDB88320;
+                }
+                else {
+                    crc = crc >> 1;
+                }
+            }
+        }
+
+        return crc;
+    }
+
+    void writeAndUpdateCRC_uint32(ostream& out, uint32_t& crc, uint32_t value) {
+        uint8_t bytes[4];
+        serialize_uint32(value, bytes);
+
+        crc = updateCRC(crc, bytes, 4);
+        write_uint32(out, bytes);
+    }
+
+    void writeAndUpdateCRC_uint64(ostream& out, uint32_t& crc, uint64_t value) {
+        uint8_t bytes[8];
+        serialize_uint64(value, bytes);
+
+        crc = updateCRC(crc, bytes, 8);
+        write_uint64(out, bytes);
     }
 
     void writePayload(ostream& out, const vector<uint8_t>& payload) {
         out.write(reinterpret_cast<const char*>(payload.data()), payload.size());
     }
 
-    uint32_t readUint32(istream& in, uint64_t offset) {
+    uint32_t readUint32(istream& in, uint64_t offset, uint32_t& crc) {
         in.seekg(static_cast<std::streamoff>(offset), std::ios::beg);
 
         unsigned char bytes[4];
@@ -63,13 +80,15 @@ namespace {
             throw std::runtime_error("failed to read uint32");
         }
 
+        crc = updateCRC(crc, bytes, 4);
+
         return (static_cast<uint32_t>(bytes[0]) << 0)  |
             (static_cast<uint32_t>(bytes[1]) << 8)  |
             (static_cast<uint32_t>(bytes[2]) << 16) |
             (static_cast<uint32_t>(bytes[3]) << 24);
     }
 
-    uint64_t readUint64(istream& in, uint64_t offset) {
+    uint64_t readUint64(istream& in, uint64_t offset, uint32_t& crc) {
         in.seekg(static_cast<std::streamoff>(offset), std::ios::beg);
 
         unsigned char bytes[8];
@@ -79,6 +98,8 @@ namespace {
         if (!in) {
             throw std::runtime_error("failed to read uint64");
         }
+
+        crc = updateCRC(crc, bytes, 8);
 
         return (static_cast<uint64_t>(bytes[0]) << 0)  |
             (static_cast<uint64_t>(bytes[1]) << 8)  |
@@ -90,29 +111,41 @@ namespace {
             (static_cast<uint64_t>(bytes[7]) << 56);
     }
     
-    uint32_t readMagic(istream& in, uint64_t offset) {
-        return readUint32(in, offset);
+    uint32_t readMagic(istream& in, uint64_t offset, uint32_t& crc) {
+        return readUint32(in, offset, crc);
     }
 
-    uint32_t readPayloadLen(istream& in, uint64_t offset){
-        return readUint32(in, offset);
+    uint32_t readPayloadLen(istream& in, uint64_t offset, uint32_t& crc){
+        return readUint32(in, offset, crc);
     }
 
-    Lsn readLsn(istream& in, uint64_t offset) {
-        return readUint64(in, offset);
+    Lsn readLsn(istream& in, uint64_t offset, uint32_t& crc) {
+        return readUint64(in, offset, crc);
     }
 
     uint32_t readCrc(istream& in, uint64_t offset){
-        return readUint32(in, offset);
+        uint32_t throwaway = initCRC();
+        return readUint32(in, offset, throwaway);
     }
 
-    const vector<uint8_t> readPayload(istream& in, uint64_t offset, const uint32_t payload_len) {
+    const vector<uint8_t> readPayload(istream& in, uint64_t offset, const uint32_t payload_len, uint32_t& crc) {
         in.seekg(static_cast<std::streamoff>(offset), std::ios::beg);
 
         vector<uint8_t> bytes(payload_len);
         in.read(reinterpret_cast<char*> (bytes.data()), payload_len);
 
+        if (!in) {
+            throw std::runtime_error("failed to read payload");
+        }
+
+        const uint8_t* tempBuffer = bytes.data();
+        crc = updateCRC(crc, tempBuffer, payload_len);
+
         return bytes;
+    }
+
+    uint32_t finalizeCRC(uint32_t crc) {
+        return crc ^ 0xFFFFFFFF;
     }
 }
 
@@ -123,12 +156,30 @@ void writeRecord(ostream& out, Lsn lsn, const vector<uint8_t>& payload) {
     if (payload.size() > UINT32_MAX)
         return;
 
+    uint32_t magic = 0x314C4157; // WAL1 - litte endian
+    uint32_t payloadLen = static_cast<uint32_t>(payload.size());
+
+    // initialize the CRC and then update after each write step.
+    uint32_t crc = initCRC();
+
     // Need to write the record data to the input file.
     // Write the data in-order per design and make sure to handle the endianess
-    writeMagic(out);
-    writePayloadLen(out, payload);
-    writeLsn(out, lsn);
-    writeCrc(out);
+    writeAndUpdateCRC_uint32(out, crc, magic);
+    writeAndUpdateCRC_uint32(out, crc, payloadLen);
+    writeAndUpdateCRC_uint64(out, crc, lsn);
+
+    if (!payload.empty()) {
+        crc = updateCRC(crc, payload.data(), payloadLen);
+    }
+
+    // finalize the CRC
+    const uint32_t finalCRC = finalizeCRC(crc);
+
+    // serialize and write the CRC
+    uint8_t bytesCRC[4]; 
+    serialize_uint32(finalCRC, bytesCRC);
+    write_uint32(out, bytesCRC);
+
     writePayload(out, payload);
 }
 
@@ -136,27 +187,36 @@ RecordReadResult readRecord(istream& in, uint64_t nextOffset) {
     RecordReadResult res;
     uint64_t offset = nextOffset;
 
-    if (readMagic(in, offset) != 0x314C4157) {
+    uint32_t crc = initCRC();
+
+    if (readMagic(in, offset, crc) != 0x314C4157) {
         res.status = RecordReadStatus::Corrupt;
         return res;
     }
     offset += SIZE_MAGIC;
 
-    uint32_t payloadLen = readPayloadLen(in, offset);
+    uint32_t payloadLen = readPayloadLen(in, offset, crc);
     offset += SIZE_PAYLOAD_LEN;
 
-    res.lsn = readLsn(in, offset);
+    res.lsn = readLsn(in, offset, crc);
     offset += SIZE_LSN;
 
-    uint32_t crc = readCrc(in, offset);
+    uint32_t dataCRC = readCrc(in, offset);
     offset += SIZE_CRC;
 
-    res.payload = readPayload(in, offset, payloadLen);
+    res.payload = readPayload(in, offset, payloadLen, crc);
     offset += res.payload.size();
 
     res.nextOffset = offset;
 
-    res.status = RecordReadStatus::Ok;
+    const uint32_t computedCRC = finalizeCRC(crc);
+
+    if (dataCRC != computedCRC) {
+        res.status = RecordReadStatus::Corrupt;
+    }
+    else {
+        res.status = RecordReadStatus::Ok;
+    }
 
     return res;
 }
